6be19f013c902bf414ce7d1f177cf6be6021a405
Qian Huang <cianhuang@outlook.com>
2019-10-31 23:07:21 -0400

    -

--- quad_solver.py
+++ quad_solver.py
@@ -6,10 +6,6 @@ def solver(curr_pos, delta_y):
     a1 = 0.04655
     a2 = -1.9481e-5
     b = 2*a2*curr_pos+a1
-    d1 = (b)**2 + 4*a2*delta_y
-    d2 = (b)**2 - 4*a2*delta_y
-    sol1 = (-b + np.sqrt(d1))/(2*a2)
+    d2 = (b)**2 + 4*a2*delta_y
     sol2 = (-b + np.sqrt(d2))/(2*a2)
-    sol3 = (-b - np.sqrt(d1))/(2*a2)
-    sol4 = (-b - np.sqrt(d2))/(2*a2)
-    return (sol1, sol2)
+    return sol2
